a4e661beb038430e1142a618fb4a1aa9351a6805
ecarnovsky <ecarnovsky@gmail.com>
2024-04-21 17:18:16 -0400

    Added a console log to the netlify func

--- netlify/functions/api-call.mjs
+++ netlify/functions/api-call.mjs
@@ -1,8 +1,11 @@
 export default async (req, context) => {
 
-    // const apiKey = Netlify.env.get("YOUTUBE_API_KEY");
-    
+    console.log("Netlify Function called at " + new Date())
 
+    // const apiKey = Netlify.env.get("YOUTUBE_API_KEY");
+
+
+    
     return new Response("Hello, world!");
   };
   
